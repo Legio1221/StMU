95031ba5ea3e99e4362533003c4c8fd6665ada10
Legio1221 <oscarg1221@gmail.com>
2015-01-06 15:40:23 -0600

    Editor logic can spit out ADV copy/paste lines for SA11.

--- Animals/src/w2FormatEditor/EditorLogic.java
+++ Animals/src/w2FormatEditor/EditorLogic.java
@@ -1,5 +1,12 @@
 package w2FormatEditor;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
 public class EditorLogic {
 
 	private int companyNum;
@@ -7,4 +14,23 @@ public class EditorLogic {
 	private int programFormatNum;
 	private String companyName;
 	final private String W2_2015 = "W2 2015";
+	
+	public EditorLogic(int companyNum, int programNum, int programFormatNum, String companyName){
+		this.companyNum = companyNum;
+		this.programNum = programNum;
+		this.programFormatNum = programFormatNum;
+		this.companyName = companyName;
+		
+	}
+	
+	public String writeW2Format(){
+		String str = "G\nSA11\nC\nPR\n" + companyNum + 
+				"\n" + programNum + "\n" 
+				+ programFormatNum + "\n" 
+				+ companyName + "\n"
+				+ W2_2015 + "\n" 
+				+ "\n";
+		
+		return str;
+	}
 }
---
+++ Animals/src/w2FormatEditor/EditorLogicTest.java
@@ -0,0 +1,21 @@
+package w2FormatEditor;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EditorLogicTest {
+	EditorLogic example;
+
+	@Before
+	public void setUp() throws Exception {
+		example = new EditorLogic(79,26,12,"Test");
+	}
+
+	@Test
+	public void testWriteW2Format() {
+		System.out.println(example.writeW2Format());
+	}
+
+}
