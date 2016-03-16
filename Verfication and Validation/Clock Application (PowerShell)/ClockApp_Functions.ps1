1281de56dec89ac51c6f2263134180aec664d968
Oscar G. Castro <oscarg_1221@hotmail.com>
2016-03-16 15:11:26 -0500

    V and V PS Script
    Verification and Validation Powershell project scripts.

--- Verfication and Validation/Clock Application (PowerShell)/ClockApp_Functions.ps1
+++ Verfication and Validation/Clock Application (PowerShell)/ClockApp_Functions.ps1
@@ -65,7 +65,7 @@ function Decrement-Minute{
 }
 #endregion
 
-#region Adjust Day-Light Savings
+#region UC3 - Adjust Day-Light Savings
 function Enable-DST{
     param([Parameter(Mandatory=$true)][DateTime]$Current_Time)
 
@@ -77,3 +77,73 @@ function Disable-DST{
 
     $Current_Time.AddHours(-1)
 }
+#endregion
+
+#region UC4 - Select Time Zone
+function Select-TimeZone{
+    #TODO
+    # Preload information into a combobox with all possible cases
+    # It's gonna be a giant switch case.
+}
+#endregion
+
+#region UC5 - View Time
+function View-Time{
+
+}
+#endregion
+
+#region UC6 - Set Alarms
+#Logic Idea
+<#
+  1. Parse necessary information
+  2. Set Alarm
+  3. profit 
+#>
+#endregion
+
+#region UC7 - Generate Alarm
+
+# The line of code below loads an alarm sound, but it isn't suitable for testing.
+#$alarm_sound = [Media.SoundPlayer]::new($PSScriptRoot + "\foo.wav")
+$alarm_sound = [Media.SoundPlayer]::new("c:\users\oscar\desktop\castro alarm clock\foo.wav")
+#endregion
+
+#region UC8 - Reset Aalarm
+function Silence-Alarm{
+    param([Parameter(Mandatory=$true)][DateTime]$Current_Time)
+}
+#endregion
+
+#region UC9 - Save User Settings / Retrieve / Parser
+<#
+$sloc = (Resolve-Path .\).Path
+$configBase = "\config.ini"
+$configLoc = $sloc + $configBase
+$config_content = Get-Content $configLoc
+$config_content
+
+$global:Time_Zone = ""
+$global:DST = 0
+$global:Time = ""
+$global:Alarm = ""
+
+foreach($line in $config_content){
+    $tempSplit = $line.split()
+    $Config_Name = $tempSplit[0]
+    $Config_Value = $tempSplit[2]
+    switch($Config_Name){
+        Time_Zone {$global:Time_Zone = $Config_Value; break}
+        DST {$global:DST = $Config_Value; break}
+        Time {$global:Time = $Config_Value + " " + $tempSplit[3..4]; break}
+        Alarm {$global:Alarm = $Config_Value + $tempSplit[3]; break}
+        default {Write-Host "Parsing Error!"; break}
+    }
+}
+Write-Host "`n`n`n"
+$global:Time_Zone
+$global:DST
+$global:Time
+$global:Alarm
+#>
+#endregion
