f16164940e7c6c8349f8758e40043226e37badb4
Oscar G. Castro <oscarg_1221@hotmail.com>
2016-04-28 17:59:15 -0500

    Update to ProxyServer.py
    see title

--- ComputerNetworking/Proxy Server/ProxyServer.py
+++ ComputerNetworking/Proxy Server/ProxyServer.py
@@ -2,7 +2,7 @@
 # @author: Oscar Guillermo Castro
 # --------------------------------------------------------
 
-from socket import import *
+from socket import *
 import sys
 from _socket import SOCK_STREAM, AF_INET
 
@@ -46,38 +46,38 @@ while 1:
         #END
             print "read from cache"
         # Error handling for file not found in Cache
-        except IOError:
-            if fileExist == "false":
-                #Create a socket on the proxy server
-                c = socket(AF_INET, SOCK_STREAM)#fill / end
-                hostn = filename.replace("www.","",1)
-                print(hostn)
-            try:
-                # Connect to the socket to port 80
-                #fill
-                c.connect(hostn,80)
-                #end
-                #Create temp file on this socket and ask port 80
-                fileobj = c.makefile('r',0)
-                fileobj.write("GET " + "http://" + filename + "HTTP/1.0\n\n")
-                # read response into buffer
-                #fill
-                #end
-                #create a new file in the cache for requested file.
-                # send response in buffer to client socket
-                tmpFile = open("./" + filename, "wb")
-                # Fill in start
-                for i in range(0,len(buff)):
-                    tmpFile.write(buff[i])
-                tcpCliSock.send(buff[i])
-                # End
-            except:
-                print "Illegal request"
-        else:
-                # HTTP response message for file not found.
-                # fill
-                print "Error 404, file not found!"
-                # end
+    except IOError:
+        if fileExist == "false":
+            #Create a socket on the proxy server
+            c = socket(AF_INET, SOCK_STREAM)#fill / end
+            hostn = filename.replace("www.","",1)
+            print(hostn)
+        try:
+            # Connect to the socket to port 80
+            #fill
+            c.connect(hostn,80)
+            #end
+            #Create temp file on this socket and ask port 80
+            fileobj = c.makefile('r',0)
+            fileobj.write("GET " + "http://" + filename + "HTTP/1.0\n\n")
+            # read response into buffer
+            #fill
+            #end
+            #create a new file in the cache for requested file.
+            # send response in buffer to client socket
+            tmpFile = open("./" + filename, "wb")
+            # Fill in start
+            for i in range(0,len(buff)):
+                tmpFile.write(buff[i])
+            tcpCliSock.send(buff[i])
+            # End
+        except:
+            print "Illegal request"
+    else:
+            # HTTP response message for file not found.
+            # fill
+            print "Error 404, file not found!"
+            # end
     # Close the client and the server sockets
     tcpCliSock.close()
 #fill
